97f32368a1f23197cce319c9488d692d7c852b72
Rich <richbport@yahoo.com>
2025-12-27 15:25:42 -0500

    moved the script tag

--- index.js
+++ index.js
@@ -9,4 +9,4 @@ function closeModal() {
 document.querySelector(".open-btn").addEventListener("click", openModal);
 document.querySelector(".close-btn").addEventListener("click", closeModal);
 
-console.log(document.querySelector(".header-btn"))
+console.log(document.querySelector(".header_btn"));
