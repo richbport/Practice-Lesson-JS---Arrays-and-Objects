d81cf28f70dc38a48dbad2af73b38836649e39b1
Rich <richbport@yahoo.com>
2025-12-27 15:02:40 -0500

    header-btn

--- index.js
+++ index.js
@@ -9,3 +9,4 @@ function closeModal() {
 document.querySelector(".open-btn").addEventListener("click", openModal);
 document.querySelector(".close-btn").addEventListener("click", closeModal);
 
+console.log(document.querySelector(".header-btn"))
