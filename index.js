2028facac52da39f89cb832293eb23a364e51aba
Rich <richbport@yahoo.com>
2025-12-29 20:49:00 -0500

    onclicks added

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 function openModal() {
-  document.querySelector(".modall").classList.add("modal--open");
+  document.querySelector(".modal").classList.add("modal--open");
 }
 
 function closeModal() {
   document.querySelector(".modal").classList.remove("modal--open");
 }
 
-document.querySelector(".open-btn").addEventListener("click", openModal);
-document.querySelector(".close-btn").addEventListener("click", closeModal);
+// document.querySelector(".open-btn").addEventListener("click", openModal);
+// document.querySelector(".close-btn").addEventListener("click", closeModal);
