dc22b626094e0b89412e76b7a4ed3d696a74d382
Rich <richbport@yahoo.com>
2026-01-03 19:24:48 -0500

    openModal adjustments

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 function openModal() {
-  document.querySelector(".modal").classList.add("modal--open")
+  console.log("open")
 }
 
 function closeModal() {
-  document.querySelector(".modal").classList.remove("modal--open");
+  console.log("close")
 }
 
 document.querySelector(".open-btn").addEventListener("click", openModal);
-document.querySelector(".close-btn").addEventListener("click", closeModal);
+
+// function openModal() {
+//   document.querySelector(".modal").classList.add("modal--open")
+// }
+
+// function closeModal() {
+//   document.querySelector(".modal").classList.remove("modal--open");
+// }
+
+// document.querySelector(".close-btn").addEventListener("click", closeModal);
